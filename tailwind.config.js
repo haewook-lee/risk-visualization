2958bc0da522112be7f4446aec0a46e854cb5e75
haiduk <20hwlee@gmail.com>
2023-04-23 02:22:37 -0400

    :rocket:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "retro",
+      {
+        mytheme: {
+          primary: "#ea5665",
+          secondary: "#e28f7c",
+          accent: "#ba2c46",
+          neutral: "#1D1622",
+          "base-100": "#EEEEF2",
+          info: "#7B9AEA",
+          success: "#178C71",
+          warning: "#FBB85B",
+          error: "#F9668E",
+        },
+      },
+    ],
+  },
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
