c7a773ddcf6e6e9a7f9c799ef2f3cd9a0f263244
haiduk <20hwlee@gmail.com>
2023-05-02 22:48:47 -0400

    map update

--- components/LeafMap.tsx
+++ components/LeafMap.tsx
@@ -16,62 +16,96 @@ type Mark = {
   Year: string
 }
 
+function LeafletCanvasMarker({ marks, years }: { marks: any; years: number }) {
+  const map = useMap()
+
+  useEffect(() => {
+    if (!map) return
+
+    console.log(years)
+
+    let ciLayer = L.canvasIconLayer({}).addTo(map) // eslint-disable-line no-use-before-define
+
+    ciLayer.addOnClickListener(function (_e: any, data: any) {
+      console.log(data)
+    })
+    ciLayer.addOnHoverListener(function (
+      e: any,
+      data: { data: { _leaflet_id: any } }[]
+    ) {
+      console.log(data[0].data._leaflet_id)
+    })
+
+    let gIcon = L.icon({
+      iconUrl:
+        "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+      iconSize: [20, 18],
+      iconAnchor: [10, 9],
+    })
+
+    let oIcon = L.icon({
+      iconUrl:
+        "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png",
+      iconSize: [20, 18],
+      iconAnchor: [10, 9],
+    })
+
+    let rIcon = L.icon({
+      iconUrl:
+        "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+      iconSize: [20, 18],
+      iconAnchor: [10, 9],
+    })
+
+    let markers = []
+
+    for (let i = 0; i < marks.length; i++) {
+      if (
+        parseInt(marks[i].Year) >= years &&
+        parseInt(marks[i].Year) <= years + 9
+      ) {
+        let icon
+        if (parseFloat(marks[i]["Risk Rating"]) <= 0.33) {
+          icon = gIcon
+        } else if (
+          parseFloat(marks[i]["Risk Rating"]) > 0.33 &&
+          parseFloat(marks[i]["Risk Rating"]) <= 0.66
+        ) {
+          icon = oIcon
+        } else {
+          icon = rIcon
+        }
+
+        let marker = L.marker(
+          [parseInt(marks[i].Lat), parseInt(marks[i].Long)],
+          {
+            icon: icon,
+          }
+        ).bindPopup(
+          marks[i]["Asset Name"] +
+            " " +
+            marks[i].Year +
+            " " +
+            marks[i]["Risk Rating"]
+        )
+        markers.push(marker)
+      }
+    }
+
+    ciLayer.addLayers(markers)
+  }, [map, marks, years])
+
+  return null
+}
+
 function Map({ marks }: any) {
   const [years, setYears] = useState<number>(2030)
 
   // Year range is 2030 - 2070
+  // Risk range is 0 - 1
 
   function changeDecades(first: number) {
     setYears(first)
-  }
-
-  function LeafletCanvasMarker() {
-    const map = useMap()
-
-    useEffect(() => {
-      if (!map) return
-
-      console.log(years)
-
-      let ciLayer = L.canvasIconLayer({}).addTo(map) // eslint-disable-line no-use-before-define
-
-      ciLayer.addOnClickListener(function (_e: any, data: any) {
-        console.log(data)
-      })
-      ciLayer.addOnHoverListener(function (
-        e: any,
-        data: { data: { _leaflet_id: any } }[]
-      ) {
-        console.log(data[0].data._leaflet_id)
-      })
-
-      let icon = L.icon({
-        iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-        iconSize: [20, 18],
-        iconAnchor: [10, 9],
-      })
-
-      let markers = []
-
-      for (let i = 0; i < marks.length; i++) {
-        if (
-          parseInt(marks[i].Year) >= years &&
-          parseInt(marks[i].Year) <= years + 9
-        ) {
-          let marker = L.marker(
-            [parseInt(marks[i].Lat), parseInt(marks[i].Long)],
-            {
-              icon: icon,
-            }
-          ).bindPopup(marks[i]["Asset Name"] + marks[i].Year)
-          markers.push(marker)
-        }
-      }
-
-      ciLayer.addLayers(markers)
-    }, [map, years])
-
-    return null
   }
 
   return (
@@ -100,7 +134,7 @@ function Map({ marks }: any) {
       <div id="map">
         <MapContainer
           className="markercluster-map"
-          center={[49.8397, 24.0297]}
+          center={[43, -79]}
           zoom={7}
           scrollWheelZoom={true}
         >
@@ -108,7 +142,7 @@ function Map({ marks }: any) {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <LeafletCanvasMarker />
+          <LeafletCanvasMarker marks={marks} years={years} />
         </MapContainer>
       </div>
     </>
